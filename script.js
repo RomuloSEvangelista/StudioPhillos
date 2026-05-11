e018d1ac0ab0813b5b1f1c238141e1eb9595857f
ROMULO DA SILVA EVANGELISTA <romulosevangelista@gmail.com>
2026-05-11 20:41:34 -0300

    corpo do site

---
+++ script.js
@@ -0,0 +1,6 @@
+// Função para garantir que os vídeos do Instagram sejam carregados corretamente
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site do Studio Phiillos carregado com sucesso!");
+    
+    
+});
